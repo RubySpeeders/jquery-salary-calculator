23307a9a0b083176c4068fddc2b1e92d50799c00
Sarah Peters <sarahnpeters@gmail.com>
2020-10-09 16:02:45 -0500

    create variables for getting employee input info

--- scripts/client.js
+++ scripts/client.js
@@ -5,5 +5,11 @@ function onReady() {
 }
 
 function addEmployeeInfo() {
-  console.log('button works');
+  //get employee information
+  const firstName = $('.js-firstName').val();
+  const lastName = $('.js-lastName').val();
+  const idNumber = $('.js-idNumber').val();
+  const jobTitle = $('.js-jobTitle').val();
+  const annualSalary = $('.js-annualSalary').val();
+  console.log(firstName, lastName, idNumber, jobTitle, annualSalary);
 }
